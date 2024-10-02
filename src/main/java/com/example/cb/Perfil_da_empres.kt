77fbc16560a344ea031c118455bbed867d704e69
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-10-02 11:03:44 -0300

    Add files via upload

---
+++ src/main/java/com/example/cb/Perfil_da_empres.kt
@@ -0,0 +1,20 @@
+package com.example.cb
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Perfil_da_empres : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perfil_da_empres)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
