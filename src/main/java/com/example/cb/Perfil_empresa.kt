4dfeb8c7c80feb27b82dece660836431eeb14550
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-11-27 10:08:02 -0300

    Update Perfil_empresa.kt

--- src/main/java/com/example/cb/Perfil_empresa.kt
+++ src/main/java/com/example/cb/Perfil_empresa.kt
@@ -1,12 +1,21 @@
 package com.example.cb
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Perfil_empresa : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
+    private lateinit var textViewNome: TextView
+    private lateinit var textViewEmail: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,38 @@ class Perfil_empresa : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        textViewNome = findViewById(R.id.textView18)
+        textViewEmail = findViewById(R.id.textView16)
+
+        val user = auth.currentUser
+        if (user != null) {
+            // Obter dados do Firestore
+            val userId = user.uid
+            getUserData(userId)
+        } else {
+            Toast.makeText(this, "Usuário não autenticado", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun getUserData(userId: String) {
+        // Referência à coleção "Dev" onde os dados do usuário estão salvos
+        firestore.collection("Empresa").document(userId).get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    val nome = document.getString("nome")
+                    val email = document.getString("email")
+
+
+                    textViewNome.text = "nome: $nome"
+                    textViewEmail.text = "email: $email"
+
+                } else {
+                    Toast.makeText(this, "Documento não encontrado", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Erro ao carregar dados: $exception", Toast.LENGTH_SHORT).show()
+            }
     }
 }
